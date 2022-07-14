35c0654dc9177f96aa0d75871e69abafbcc98b8b
drc9472 <108583927+drc9472@users.noreply.github.com>
2022-07-14 08:42:58 -0400

    Added user validation and logic to determine the winner.

--- game.py
+++ game.py
@@ -1,29 +1,50 @@
 import random
 
 
-print("WELCOME TO ROCK PAPER SCISSORS")
+print("Welcome to my rock, paper, scissors game!")
 
 
 # USER INPUT
 user_choice = input("Please make your selection ('rock', 'paper', 'scissors'):")
-print(f"You chose: '{user_choice.capitalize()}'")
+user_choice = user_choice.capitalize()
+print(f"You chose: '{user_choice}'")
 
 
 # VALIDATE USER INPUTS
-
-
-
+valid_choice = ["Rock", "Paper", "Scissors"]
+if user_choice not in valid_choice:
+    print("Your choice is invalid")
+    exit()
 
 
 # COMPUTER CHOICE
-valid_choice = ["Rock", "Paper", "Scissors"]
 computer_choice = random.choice(valid_choice)
 print(f"The computer chose: '{computer_choice}'")
 
 
 
 # DETERMINE THE WINNER
+# Adapted from code shared in slack by Bonnie:
+#https://nyu-tech-2335.slack.com/archives/C5WPFSB52/p1657672686150239
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif user_choice == "Rock":
+    if computer_choice == "Scissors":
+        print("Rock crushes scissors. You win!")
+    else:
+        print("Paper covers rock. You lose.")
+elif user_choice == "Paper":
+    if computer_choice == "Rock":
+        print("Paper covers rock. You win!")
+    else:
+        print("Scissors cuts paper. You lose.")
+elif user_choice == "Scissors":
+    if computer_choice == "Paper":
+        print("Scissors cuts paper. You win!")
+    else:
+        print("Rock crushes scissors. You lose.")
 
+print("Thank you for playing. Please play again!")
 
 # DISPLAY RESULTS
 
